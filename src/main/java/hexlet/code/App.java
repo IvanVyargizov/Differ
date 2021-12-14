435fc2e0de8eaa1abca6806638d639e913e32586
Ivan_Vyargizov <vyargizov@gmail.com>
2021-12-14 21:42:48 +0400

    add help information using picocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,27 @@
 package hexlet.code;
 
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command (
+        mixinStandardHelpOptions = true,
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Callable<Integer> {
+
+  public static void main (String[] args) {
+    int exitCode = new CommandLine(new App()).execute(args);
+    System.exit(exitCode);
   }
+
+  @Override
+  public Integer call() {
+    System.out.println("Hello World!");
+    return 0;
+  }
+
 }
 
