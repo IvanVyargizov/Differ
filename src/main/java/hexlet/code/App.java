5359564dc028e6e162beebd507015f9d42025526
Ivan_Vyargizov <vyargizov@gmail.com>
2021-12-19 18:35:52 +0400

    added file matching logic

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.lang.Exception;
 
 import java.util.concurrent.Callable;
 
@@ -12,16 +15,24 @@ import java.util.concurrent.Callable;
 )
 public class App implements Callable<Integer> {
 
+  @Option (names = {"-f", "--format"}, description = "output format [default: stylish]")
+  private String format;
+
+  @Parameters(paramLabel = "filepath1", description = "path to first file")
+  private String filepath1;
+
+  @Parameters(paramLabel = "filepath2", description = "path to second file")
+  private String filepath2;
+
+  @Override
+  public Integer call() throws Exception {
+    System.out.println(Differ.generate(filepath1, filepath2));
+    return 0;
+  }
+
   public static void main (String[] args) {
     int exitCode = new CommandLine(new App()).execute(args);
     System.exit(exitCode);
   }
 
-  @Override
-  public Integer call() {
-    System.out.println("Hello World!");
-    return 0;
-  }
-
 }
-
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,65 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class Differ{
+
+    public static String generate(String path1, String path2) throws IOException {
+        HashMap<String, String> file1 = read(path1);
+        HashMap<String, String> file2 = read(path2);
+        TreeMap<String, String> mergingFile = new TreeMap<>(file1);
+        file2.forEach(
+                (key, value) -> mergingFile.merge(key, value, (value1, value2) -> value2)
+        );
+        LinkedList<String> diff = new LinkedList<>();
+        mergingFile.forEach(
+                (key, value) -> {
+                    if (!file1.containsKey(key) && file2.containsKey(key)) {
+                        diff.add("  + " + key + ": " + value);
+                    } else if (file1.containsKey(key) && !file2.containsKey(key)) {
+                        diff.add("  - " + key + ": " + value);
+                    } else if (!value.equals(file1.get(key))) {
+                        diff.add("  - " + key + ": " + file1.get(key));
+                        diff.add("  + " + key + ": " + value);
+                    } else diff.add("    " + key + ": " + value);
+                }
+        );
+        return "{\n" + String.join("\n", diff) + "\n}";
+    }
+
+    private static HashMap<String, String> read(String path) throws IOException {
+        String absolutePath = castAbsolutePath(path);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(new File(absolutePath));
+        String fileContent = jsonNode.toString();
+        return objectMapper.readValue(fileContent, new TypeReference<>(){});
+    }
+
+    private static String castAbsolutePath (String path) throws IOException {
+        Path checking = Paths.get(path);
+        if (checking.isAbsolute()) {
+            return path;
+        }
+        Path rootPath = Paths.get("/home");
+        return Files.find(rootPath,
+                Integer.MAX_VALUE,
+                (p, basicFileAttributes) ->
+                        p.getFileName().toString().equals(path))
+                .collect(Collectors.toList()).toString()
+                .replaceAll("\\[", "")
+                .replaceAll("]", "");
+    }
+
+}
