3e68a8f22f9f064190aede579f16dd4e55a4e0c1
Ivan_Vyargizov <vyargizov@gmail.com>
2022-01-01 22:22:38 +0400

    refactoring

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,32 +1,23 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class Differ {
 
     public static String generate(String path1, String path2) {
         HashMap<String, String> file1;
         try {
-            file1 = read(path1);
+            file1 = Parser.read(path1);
         } catch (IOException ioe) {
             return "Incorrect path to first file. No such file or directory";
         }
 
         HashMap<String, String> file2;
         try {
-            file2 = read(path2);
+            file2 = Parser.read(path2);
         } catch (IOException ioe) {
             return "Incorrect path to second file. No such file or directory";
         }
@@ -54,27 +45,6 @@ public class Differ {
                 }
         );
         return diff;
-    }
-
-    private static HashMap<String, String> read(String path) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(new File(castAbsolutePath(path)));
-        String fileContent = jsonNode.toString();
-        return objectMapper.readValue(fileContent, new TypeReference<>() { });
-    }
-
-    private static String castAbsolutePath(String path) throws IOException {
-        if (Paths.get(path).isAbsolute()) {
-            return path;
-        }
-        Path rootPath = Paths.get(path).toAbsolutePath().getRoot().resolve(Paths.get(path).toAbsolutePath().getName(0));
-        return Files.find(rootPath,
-                Integer.MAX_VALUE,
-                (p, basicFileAttributes) ->
-                        p.endsWith(Paths.get(path)))
-                .collect(Collectors.toList()).toString()
-                .replaceAll("\\[", "")
-                .replaceAll("]", "");
     }
 
 }
